9f195260cd061ff244954cbbb78a690d0a350d61
Aamir Ali <aamirali.dev@gmail.com>
2024-12-01 17:29:20 +0500

    enabled netlify function

---
+++ functions/server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("../config/db");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const errorHandler = require("../middlewares/error");
+const bodyParser = require("body-parser");
+const path = require("path");
+const serverless = require("serverless-http");
+
+// Route files
+const auth = require("../routes/auth");
+
+// Load env vars
+dotenv.config({ path: "../.env" });
+
+// Connect database
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Body parser
+app.use(
+    bodyParser.json({
+        limit: "50mb",
+    })
+);
+app.use(
+    bodyParser.urlencoded({
+        limit: "50mb",
+        extended: true,
+    })
+);
+app.use(cors());
+
+// Enable CORS
+app.use(cors());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+app.use(express.static(path.join(__dirname, "../HR-Management-FE/dist/")));
+
+// Mount Routers
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/onboard", require("../routes/onboard"));
+app.use("/api/v1/roles", require("../routes/roles"));
+app.use("/api/v1/employee", require("../routes/employee"));
+app.use("/api/v1/timeLog", require("../routes/timeLog"));
+app.use("/api/v1/timeOff", require("../routes/timeOff"));
+app.use("/api/v1/setting", require("../routes/setting"));
+
+app.get("*", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "../HR-Management-FE/dist/", "index.html")
+    );
+});
+
+// Error handler
+app.use(errorHandler);
+
+app.use("/", (req, res) => {
+    res.status(404).json({ success: false, msg: "Page not found" });
+});
+
+// Start standalone server if not running in a serverless environment
+if (process.env.NETLIFY_DEV !== "true") {
+    const server = app.listen(PORT, () => {
+        console.log(
+            `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+                .bgGreen.bold
+        );
+    });
+
+    // Handle unhandled promise rejections
+    process.on("unhandledRejection", (err, promise) => {
+        console.log(`Error : ${err}`.red);
+        server.close(() => process.exit(1));
+    });
+}
+module.exports.handler = serverless(app);
--- middlewares/uploadFile.js
+++ middlewares/uploadFile.js
@@ -3,48 +3,48 @@ const multerS3 = require("multer-s3");
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID_OUR,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_OUR,
+    region: process.env.AWS_REGION_OUR,
 });
 
 function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif|pdf/;
-  // Check ext
-  const extname = filetypes.test(
-    file.originalname.toLowerCase().split(".").pop()
-  );
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif|pdf/;
+    // Check ext
+    const extname = filetypes.test(
+        file.originalname.toLowerCase().split(".").pop()
+    );
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
 
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images Only!");
-  }
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb("Error: Images Only!");
+    }
 }
 
 const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.AWS_S3_BUCKET,
-    acl: "public-read",
-    metadata: function (req, file, cb) {
-      cb(null, { fieldName: file.fieldname });
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_S3_BUCKET,
+        acl: "public-read",
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, `${Date.now().toString()}-${file.originalname}`);
+        },
+    }),
+    limits: { fileSize: 330000000 }, // 20 MB
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
     },
-    key: function (req, file, cb) {
-      cb(null, `${Date.now().toString()}-${file.originalname}`);
+    onError: function (err, next) {
+        console.log("error", err);
+        next(err);
     },
-  }),
-  limits: { fileSize: 330000000 }, // 20 MB
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  },
-  onError: function (err, next) {
-    console.log("error", err);
-    next(err);
-  },
 });
 
 module.exports = upload;
