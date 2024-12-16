3b7ad4fe56f3ff00fae41117162ec436ecaa4056
Rehan4778 <rehanashraf4778@gmail.com>
2024-12-16 12:25:35 +0500

    Added forget password functionality

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const {
   register,
   login,
-  //   getMe,
-  //   updateDetails,
-  //   updatePassword,
-  //   forgetPassword,
-  //   resetPassword,
-  //   logout,
+  selectCompany,
+  logout,
+  addEmployee,
+  forgetPassword,
+  resetPassword,
 } = require("../controllers/auth");
 // const { protect } = require("../middlewares/auth");
 
@@ -18,8 +17,7 @@ router.route("/:domain/login").post(login);
 // router.route("/me").get(protect, getMe);
 // router.route("/updatedetails").put(protect, updateDetails);
 // router.route("/updatepassword").put(protect, updatePassword);
-// router.route("/forgetpassword").post(forgetPassword);
-// router.route("/resetpassword/:resettoken").put(resetPassword);
-// router.route("/logout").get(logout);
+router.route("/forgetpassword").post(forgetPassword);
+router.route("/resetpassword/:resettoken").put(resetPassword);
 
 module.exports = router;
